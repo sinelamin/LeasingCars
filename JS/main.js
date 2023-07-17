e6197154630b45385a17086ef20e3f90ba52dbef
Alexey Mikhaylov <krummi1414@yandex.ru>
2023-07-17 16:13:02 +0300

    feat: add page 'new-cars' and tabs on js

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,32 @@
 "use strict";
 
+
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
+
+tabItem.forEach(item => {
+  item.addEventListener('click', open);
+});
+
+function open(e) {
+  const tabTarget = e.currentTarget;
+  const button = tabTarget.dataset.button;
+
+  tabItem.forEach(item => {
+    item.classList.remove('tabs__btn-item--active');
+  });
+  
+  tabTarget.classList.add('tabs__btn-item--active');
+
+  tabContent.forEach(item => {
+    item.classList.remove('tabs__content-item--active');
+  });
+
+  document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+}
+
+
+
 const swiper = new Swiper(".swiper", {
   effect: "fade",
   pagination: {
